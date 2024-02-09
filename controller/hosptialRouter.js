77c9e142a9cc2d7b49c4f410ae83f178df3e044f
aswathyappukuttan2001 <aswathyappukuttan2019>
2024-02-09 09:07:10 +0530

    search

--- controller/hosptialRouter.js
+++ controller/hosptialRouter.js
@@ -22,10 +22,12 @@ router.get("/view",async(req,res)=>{
 })
 
 
+
 router.post("/search",async(req,res)=>{
     let input=req.body
     let data=await hosptialModel.find(input) 
     res.json(data)
 })
+
 
 module.exports=router
